f993202bbf74e3059edd199a78f2c72951d22142
huyho147 <beobeosusu0511@gmail.com>
2024-07-03 20:08:10 +0700

    sua

--- DayFibonacci.cpp
+++ DayFibonacci.cpp
@@ -3,14 +3,12 @@ using namespace std;
 int main()
 {
 	cout << "Day Fibonacci!!!\n";
-	int x1 = 0, x2 = 1;
-	int i = 0;
-	while (i<=20)
+	int x1 = 1, x2 = 1;
+	for(int i = 0; i < 20; i++)
 	{	
 		cout << x1 << "|";
 		int tong = x1 + x2;
 		x1 = x2;
 		x2 = tong;
-		i++;
 	}
 }
